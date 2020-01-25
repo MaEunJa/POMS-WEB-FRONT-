68b2b851bb28e6b82da335b213afb75f840b2e15
MaEunJa <ttochi0115@gmail.com>
2020-01-25 14:42:00 +0900

    real url

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,11 +1,11 @@
 import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
-
+//console.log(process.env.NODE_ENV)
 export default new ApolloClient({
   uri:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000"
-      : "http://localhost:4000",
+      : "https://poms-test-backend.herokuapp.com",
   clientState: {
     defaults,
     resolvers
