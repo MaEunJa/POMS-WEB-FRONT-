d0c76f5b2a1167beabf11e49e241651068142f50
MaEunJa <ttochi0115@gmail.com>
2020-04-14 19:59:00 +0900

    buf fix 2

--- src/Routes/ParkingStatus/ParkingStatusContainer.js
+++ src/Routes/ParkingStatus/ParkingStatusContainer.js
@@ -6,17 +6,14 @@ import ParkingStatusPresenter from "./ParkingStatusPresenter";
 
 const GET_PARKINGSTATUS = gql`
   query allParkingStatus() {
-<<<<<<< HEAD
     ParkingStatus() {
-=======
->>>>>>> 0a320f0daa80340f7c660a015ebd2996273486e9
       id
       floorCode
       floorName
       totalSpace
       occupied
   }
-`;
+}`;
 
 export default withRouter(() => {
   const { data, loading } = useQuery(GET_PARKINGSTATUS);
