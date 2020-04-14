e47e35c495c1c806099b8dfa9fb25fc8c9bc3601
MaEunJa <ttochi0115@gmail.com>
2020-04-14 10:49:35 +0900

    test err4

---
+++ src/Routes/ParkingStatus/ParkingStatusContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import withRouter from "react-router-dom/withRouter";
+import { useQuery, useMutation } from "react-apollo-hooks";
+import ParkingStatusPresenter from "./ParkingStatusPresenter";
+
+const GET_PARKINGSTATUS = gql`
+  query allParkingStatus() {
+    allParkingStatus() {
+      id
+      floorCode
+      floorName
+      totalSpace
+      occupied
+    }
+  }
+`;
+
+export default withRouter(() => {
+  const { data, loading } = useQuery(GET_PARKINGSTATUS);
+  return <ParkingStatusPresenter loading={loading} data={data} />;
+});
